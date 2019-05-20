8ca1183b4bf672a40084a03d6b982b0197d1543b
bart <bartvp@live.nl>
2019-05-20 13:37:28 +0200

    Update Artikel.java

--- src/Artikel.java
+++ src/Artikel.java
@@ -1,2 +1,29 @@
 public class Artikel {
+
+    private String naam;
+    private float prijs;
+
+    public Artikel(String naam, float prijs){
+        this.naam = naam;
+        this.prijs = prijs;
+    }
+
+    public Artikel(){
+    }
+
+    public String getNaam(){
+        return naam;
+    }
+
+    public float getPrijs(){
+        return prijs;
+    }
+
+    public void setNaam(String nieuw){
+        naam = nieuw;
+    }
+
+    public void setPrijs(float nieuw){
+        prijs = nieuw;
+    }
 }
